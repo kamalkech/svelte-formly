9f004feb8fa20ef923f340730f9bbb6435d417ca
Kamal Sahmoud <maroc.develop@gmail.com>
2019-11-22 09:58:24 +0100

    CLear files

--- index.js
+++
@@ -1,6 +0,0 @@
-import Field from "./src/Components/Field.svelte";
-import { valuesForm } from "./src/Components/stores";
-import { form, bindClass } from "./src/Validation";
-
-export { Field, valuesForm, form, bindClass };
-export const formly = { Field, valuesForm, form, bindClass };
